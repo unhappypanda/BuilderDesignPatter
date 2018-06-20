6ae11c6976b26ee58a2014daa0352a05f911ad12
Andrei Elvedi <andrei.elvedi@tss-yonder.com>
2018-06-20 12:09:37 +0300

    Uploaded project

---
+++ BuilderExample/BuilderExample/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BuilderExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sandwich sandwitch1 = new Sandwich();
+
+            sandwitch1.BreadType = BreadType.Wheat;
+            sandwitch1.CheeseType = CheeseType.Cheddar;
+            sandwitch1.MeatType = MeatType.Ham;
+            sandwitch1.hasMayo = true;
+            sandwitch1.isToasted = true;
+            sandwitch1.hasMustard = true;
+            sandwitch1.Vegetables = new List<string>{ "Tomato", "Onion" };
+            
+            sandwitch1.Display();
+
+
+            Sandwich sandwitch2 = new Sandwich();
+
+            sandwitch2.BreadType = BreadType.Wheat;
+            sandwitch2.CheeseType = CheeseType.Cheddar;
+            sandwitch2.MeatType = MeatType.Turkey;
+            sandwitch2.hasMayo = true;
+            sandwitch2.isToasted = true;
+            sandwitch2.hasMustard = true;
+            sandwitch2.Vegetables = new List<string> { "Tomato", "Onion" };
+            
+            sandwitch2.Display();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ BuilderExample/BuilderExample/Sandwich.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BuilderExample
+{
+    public class Sandwich
+    {
+        public BreadType BreadType { get; set; }
+        public bool isToasted { get; set; }
+        public bool hasMayo { get; set; }
+        public bool hasMustard { get; set; }
+        public CheeseType CheeseType { get; set; }
+        public MeatType MeatType { get; set; }
+        public List<string> Vegetables { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine("Bread type: {0}", BreadType);
+            if (isToasted)
+                Console.WriteLine("is toasted");
+            if (hasMayo)
+                Console.WriteLine("has mayo");
+            if (hasMustard)
+                Console.WriteLine("has mustard");
+            Console.WriteLine("Cheese type: {0}", CheeseType);
+            Console.WriteLine("Meat type: {0}", MeatType);
+            Console.WriteLine("Vegetables:");
+            foreach (string vegetable in Vegetables)
+            {
+                Console.WriteLine("{0}", vegetable);
+            }
+            Console.WriteLine("\n");
+        }
+    }
+
+    public enum BreadType
+    {
+        Wheat,
+        white
+    }
+
+    public enum CheeseType
+    {
+        Cheddar,
+        Swiss
+    }
+
+    public enum MeatType
+    {
+        Salami,
+        Turkey,
+        Ham
+    }
+}
